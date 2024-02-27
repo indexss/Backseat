a2299bbf6702f4d251b0c5b09475fa42d6ef2f56
Abigail Pain <txp271@student.bham.ac.uk>
2024-02-27 17:17:21 +0000

    Revert "Regenderate JDL"
    This reverts commit 3a5d618ee48f9bc3ff11009b2a8edf02d9a6bcb7.

--- src/main/webapp/app/entities/follow/follow.model.ts
+++
@@ -1,7 +0,0 @@
-export interface IFollow {
-  id: number;
-  sourceUserID?: string | null;
-  targetUserID?: string | null;
-}
-
-export type NewFollow = Omit<IFollow, 'id'> & { id: null };
--- src/main/webapp/app/entities/setting/setting.model.ts
+++
@@ -1,8 +0,0 @@
-export interface ISetting {
-  id: number;
-  userID?: string | null;
-  key?: string | null;
-  value?: string | null;
-}
-
-export type NewSetting = Omit<ISetting, 'id'> & { id: null };
