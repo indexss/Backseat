788f82586715776255be3c6ca18a71570b11abcd
Hao Li <106hxl@student.bham.ac.uk>
2024-03-19 21:55:07 +0000

    back end - impl new service and api

---
+++ src/main/java/team/bham/service/impl/WantToListenListServiceImpl.java
@@ -0,0 +1,41 @@
+package team.bham.service.impl;
+
+import java.util.List;
+import javax.annotation.Resource;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.bham.domain.WantToListenListEntry;
+import team.bham.repository.AlbumRepository;
+import team.bham.repository.ArtistRepository;
+import team.bham.repository.TrackRepository;
+import team.bham.repository.WantToListenListEntryRepository;
+import team.bham.service.WantToListenListService;
+
+@Service
+@Transactional
+public class WantToListenListServiceImpl implements WantToListenListService {
+
+    @Resource
+    private WantToListenListEntryRepository wantListRepository;
+
+    @Resource
+    private TrackRepository trackRepository;
+
+    @Resource
+    private AlbumRepository albumRepository;
+
+    @Resource
+    private ArtistRepository artistRepository;
+
+    @Override
+    public List<WantToListenListEntry> fetchAllWantToListenList() {
+        System.out.println("******************out*******************\n" + wantListRepository.findAllWantList());
+        return wantListRepository.findAllWantList();
+    }
+
+    @Override
+    public List<WantToListenListEntry> fetchUserWantToListenList(@Param("User ID") String userID) {
+        return wantListRepository.findAllByUserIDOrderByAddTimeAsc(userID);
+    }
+}
--- src/main/webapp/app/want-to-listen/list-view/list-view.component.ts
+++ src/main/webapp/app/want-to-listen/list-view/list-view.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import format from '@popperjs/core/lib/utils/format';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import getDocumentElement from '@popperjs/core/lib/dom-utils/getDocumentElement';
+import { IWantToListenListEntry } from '../../entities/want-to-listen-list-entry/want-to-listen-list-entry.model';
+import { HttpResponse } from '@angular/common/http';
+import { WantToListenListEntryService } from '../../entities/want-to-listen-list-entry/service/want-to-listen-list-entry.service';
+
+import { HttpClient } from '@angular/common/http';
+import { SortService } from '../../shared/sort/sort.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 interface listItem {
   id: number;
@@ -46,13 +55,59 @@ export class ListViewComponent implements OnInit {
     },
   ];
 
-  constructor() {}
+  showDeleteIcon: boolean = false;
 
-  ngOnInit(): void {}
+  faTrash = faTrash;
+  constructor(
+    protected activatedRoute: ActivatedRoute,
+    private wantToListenListEntryService: WantToListenListEntryService,
+    public router: Router,
+    protected http: HttpClient,
+    protected sortService: SortService,
+    protected modalService: NgbModal
+  ) {}
 
-  focus(e: Event): void {
-    // On mouse over
+  ngOnInit(): void {
+    this.loadAll();
+    console.log(this.itemList);
   }
+  loadAll(): void {
+    this.http.get('api/want-to-listen-list-entries');
+  }
+
+  //更改focus状态
+  // focus(e: Event): void {
+  //   // On mouse over
+  // }
+  onMouseEnter(): void {
+    this.showDeleteIcon = true;
+  }
+
+  onMouseLeave(): void {
+    this.showDeleteIcon = false;
+  }
+  deleteItem(id: number): void {
+    const confirmDeletion = confirm('Are you sure you want to delete this item?');
+    if (confirmDeletion) {
+      this.wantToListenListEntryService.delete(id).subscribe({
+        next: () => {
+          console.log('Deletion successful');
+          this.loadAll(); // 重新加载列表以反映删除
+        },
+        error: () => {
+          console.error('There was an error deleting the item');
+          // 这里可以添加更多用户友好的错误处理逻辑
+        },
+      });
+    }
+  }
+  // showDeleteNotification(deletedItem: listItem): void {
+  //   alert(`Item ${deletedItem.trackName} deleted`);
+  //   setTimeout(() => {
+  //     console.log("The option to undo the deletion has expired.");
+  //   }, 3000); // 模拟3秒后删除提示消失
+  // }
+
   expand(e: Event): void {
     //On mouse click
   }
