987f9c2504820bb06f0ea58ec90abaf0cce58570
Hao Li <106hxl@student.bham.ac.uk>
2024-04-08 01:29:17 +0100

    "add new" component completed, "want-to-listen-list/post" api completed

--- src/main/java/team/bham/web/rest/WantToListenListController.java
+++ src/main/java/team/bham/web/rest/WantToListenListController.java
@@ -1,12 +1,17 @@
 package team.bham.web.rest;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import java.time.Instant;
 import java.util.List;
 import javax.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import team.bham.domain.WantToListenListEntry;
+import team.bham.service.WantToListenListEntryService;
 import team.bham.service.WantToListenListService;
+import team.bham.service.dto.WantToListenListEntryDTO;
 import team.bham.utils.ResponseUtils;
 
 @RestController
@@ -15,6 +20,8 @@ public class WantToListenListController {
 
     @Resource
     private WantToListenListService wantListService;
+
+    private WantToListenListEntryService entryService;
 
     @GetMapping("/all")
     public ResponseUtils fetchAllListEntries() {
@@ -43,6 +50,29 @@ public class WantToListenListController {
             resp = new ResponseUtils().put("test", json);
         } catch (Exception e) {
             System.out.println("Testing Error");
+            resp = new ResponseUtils(e.getClass().getSimpleName(), e.getMessage());
+        }
+        return resp;
+    }
+
+    @GetMapping("/post")
+    public ResponseUtils addWantToListenListEntry(@RequestParam String spotifyURI, @RequestParam String userID) {
+        System.out.println("**********************Get api want-to-listen/post request*************************");
+        System.out.println("spotifyURI: " + spotifyURI);
+        System.out.println("userID: " + userID);
+        System.out.println("**********************************************************************************");
+        ResponseUtils resp;
+
+        WantToListenListEntryDTO entryDTO = new WantToListenListEntryDTO();
+        Instant now = Instant.now();
+        entryDTO.setSpotifyURI(spotifyURI);
+        entryDTO.setUserID(userID);
+        entryDTO.setAddTime(now);
+        try {
+            entryService.save(entryDTO);
+            resp = new ResponseUtils().put("Adding Complete", now.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
             resp = new ResponseUtils(e.getClass().getSimpleName(), e.getMessage());
         }
         return resp;
--- src/main/webapp/app/want-to-listen/add-new-want-to-listen-item/add-new-want-to-listen-item.component.ts
+++ src/main/webapp/app/want-to-listen/add-new-want-to-listen-item/add-new-want-to-listen-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { faPlayCircle, faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'jhi-add-new-want-to-listen-item',
@@ -7,14 +8,22 @@ import { faPlayCircle, faPlusSquare } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./add-new-want-to-listen-item.component.scss'],
 })
 export class AddNewWantToListenItemComponent implements OnInit {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {}
 
   @Input() itemURI!: string;
+  @Input() userID!: string;
 
-  protected addNewItem(itemURI: string): void {
-    console.log('adding ' + itemURI);
+  protected addNewItem(): void {
+    const body = {
+      spotifyURI: this.itemURI,
+      userID: this.userID,
+    };
+    // sending http request
+    // this.http.post("api/want-to-listen-list/post", body)
+    console.log('Adding item: ' + this.itemURI + 'to User: ' + this.userID);
+    console.log(body);
   }
 
   protected readonly faPlusSquare = faPlusSquare;
