733c6c6da814f76773e999ef0048c0af7cbc8b89
Finn <yxs152@student.bham.ac.uk>
2024-04-13 22:40:26 +0100

    change into branch rating

--- src/main/webapp/app/search/search.component.ts
+++ src/main/webapp/app/search/search.component.ts
@@ -6,7 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
+  searchText: any;
+  heroes = [
+    { id: 11, name: 'Mr. Nice', country: 'India' },
+    { id: 12, name: 'Narco', country: 'USA' },
+    { id: 13, name: 'Bombasto', country: 'UK' },
+    { id: 14, name: 'Celeritas', country: 'Canada' },
+    { id: 15, name: 'Magenta', country: 'Russia' },
+    { id: 16, name: 'RubberMan', country: 'China' },
+    { id: 17, name: 'Dynama', country: 'Germany' },
+    { id: 18, name: 'Dr IQ', country: 'Hong Kong' },
+    { id: 19, name: 'Magma', country: 'South Africa' },
+    { id: 20, name: 'Tornado', country: 'Sri Lanka' },
+  ];
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log('ksdj1');
+  }
 }
