082fa93ebb72cc50bf1cf30c69ccc9cae5449e68
exg046 <erose1627@gmail.com>
2024-04-16 21:47:25 +0100

    Stop folders from being duplicated in the display

--- src/main/java/team/bham/service/impl/ExploreFoldersServiceImpl.java
+++ src/main/java/team/bham/service/impl/ExploreFoldersServiceImpl.java
@@ -1,5 +1,6 @@
 package team.bham.service.impl;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.Resource;
@@ -26,10 +27,16 @@ public class ExploreFoldersServiceImpl implements ExploreFoldersService {
         List<Folder> allFolders = folderRepository.findAll();
         List<Folder> chosenFolders = new ArrayList<>() {};
 
+        ArrayList<Long> numbers = new ArrayList<Long>();
+
         for (int i = 0; i < 10; i++) {
             long lowLimit = 0;
             long highLimit = allFolders.size();
             long random_long = lowLimit + (long) (Math.random() * (highLimit - lowLimit));
+            while (numbers.contains(random_long)) {
+                random_long = lowLimit + (long) (Math.random() * (highLimit - lowLimit));
+            }
+            numbers.add(random_long);
             Folder oneFolder = folderRepository.getById(random_long);
             chosenFolders.add(oneFolder);
         }
