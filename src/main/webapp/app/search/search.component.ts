a91f94296f15d428201d0bb40e4f671cd9520fe6
Abigail Pain <txp271@student.bham.ac.uk>
2024-04-17 01:30:41 +0100

    Add fundamentals of search page layout

--- src/main/webapp/app/search/search.component.ts
+++ src/main/webapp/app/search/search.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
+
+  protected loading: boolean = true;
+
   constructor() {}
 
   ngOnInit(): void {}
