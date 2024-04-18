72738a240cdd2a9b225a84f41a214db1c2e5262e
Abigail Pain <txp271@student.bham.ac.uk>
2024-04-18 16:16:56 +0100

    Undo formatting changes

--- src/main/java/team/bham/spotify/responses/SearchResponse.java
+++ src/main/java/team/bham/spotify/responses/SearchResponse.java
@@ -5,13 +5,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SearchResponse {
-
     @JsonProperty("tracks")
     public TrackBox tracks;
 
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class TrackBox {
-
         @JsonProperty("items")
         public TrackResponse[] items;
     }
