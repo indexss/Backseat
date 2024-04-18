4f05a6ba2bff484dfb08bef71abecb75fe8d2d53
exg046 <erose1627@gmail.com>
2024-04-18 14:16:34 +0100

    Fix ordering of tracks and albums in Recently Reviewed

--- src/main/java/team/bham/service/impl/RecentlyReviewedServiceImpl.java
+++ src/main/java/team/bham/service/impl/RecentlyReviewedServiceImpl.java
@@ -42,6 +42,7 @@ public class RecentlyReviewedServiceImpl implements RecentlyReviewedService {
         System.out.println("rrByTrackName: " + rrByTrackName);
         List<Track> tracks = new ArrayList<>();
         List<Album> albums = new ArrayList<>();
+        List<String> both = new ArrayList<>();
 
         int x = 0;
         while (x < rrByTrackName.size() && x < 50) {
@@ -50,16 +51,53 @@ public class RecentlyReviewedServiceImpl implements RecentlyReviewedService {
                     String rrName = rrByTrackName.get(j).getTrack().getName();
                     Track trackName = trackRepository.fetchTrackbyRecentReview(rrName);
                     tracks.add(trackName);
+                    both.add("Track");
                 } else {
                     String rrAlbumName = rrByTrackName.get(j).getAlbum().getName();
                     Album albumName = albumRepository.fetchAlbumbyRecentReview(rrAlbumName);
                     albums.add(albumName);
+                    both.add("Album");
                 }
                 //System.out.println("tracks: " + tracks);
                 x += 1;
                 //System.out.println("x: " + x);
             }
         }
+        int trackCounter = 0;
+        int albumCounter = 0;
+        for (int i = 0; i < (tracks.size() + albums.size()); i++) {
+            RecentlyReviewedDTO rrDTO = new RecentlyReviewedDTO();
+            rrDTO.setId(i + 1);
+            if (both.get(i) == "Track") {
+                rrDTO.setTrackURI(tracks.get(trackCounter).getSpotifyURI());
+                rrDTO.setTrackName(tracks.get(trackCounter).getName());
+                rrDTO.setAlbum(tracks.get(trackCounter).getAlbum().getName());
+                rrDTO.setRating(tracks.get(trackCounter).getRating());
+                rrDTO.setImgURL(tracks.get(trackCounter).getAlbum().getImageURL());
+                Set<Artist> artistSet = tracks.get(trackCounter).getArtists();
+                List<Artist> artistList = new ArrayList<>(artistSet);
+                rrDTO.setArtist(artistList.get(0).getName());
+                Set<Review> byTrackSpotifyURI = reviewRepository.findByTrackSpotifyURI(rrDTO.getTrackURI());
+                rrDTO.setReviews(byTrackSpotifyURI.size());
+                recentTrackDTO.add(rrDTO);
+                trackCounter += 1;
+            } else {
+                rrDTO.setAlbumURI(albums.get(albumCounter).getSpotifyURI());
+                rrDTO.setAlbum(albums.get(albumCounter).getName());
+                rrDTO.setRating(albums.get(albumCounter).getRating());
+                rrDTO.setImgURL(albums.get(albumCounter).getImageURL());
+                Set<Artist> artistSet = albums.get(albumCounter).getArtists();
+                List<Artist> artistList = new ArrayList<>(artistSet);
+                rrDTO.setArtist(artistList.get(0).getName());
+                Set<Review> byAlbumSpotifyURI = reviewRepository.findByAlbumSpotifyURI(rrDTO.getAlbumURI());
+                rrDTO.setReviews(byAlbumSpotifyURI.size());
+                //System.out.println("rrDTO: " + rrDTO);
+                recentTrackDTO.add(rrDTO);
+                albumCounter += 1;
+            }
+        }
+
+        /*
         for (int i = 0; i < tracks.size(); i++) {
             RecentlyReviewedDTO rrDTO = new RecentlyReviewedDTO();
             rrDTO.setId(i + 1);
@@ -94,7 +132,7 @@ public class RecentlyReviewedServiceImpl implements RecentlyReviewedService {
             recentTrackDTO.add(rrDTO);
             //System.out.println("recentTrackDTO: " + recentTrackDTO);
         }
-
+        */
         return recentTrackDTO;
     }
 }
